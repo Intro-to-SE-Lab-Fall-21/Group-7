fcf14f504dc4ea95dc34952150a97bfae143f6dc
Jonathan Harjono <59001247+JoSky21@users.noreply.github.com>
2021-09-30 19:14:07 -0500

    Add files via upload
    Created app folder

---
+++ app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BaseTypeComponent } from './base-type/base-type.component';
+import { SignInComponent } from './base-type/sign-in/sign-in.component';
+import { EmailListComponent } from './base-type/email-list/email-list.component';
+import { GmailService } from './shared/gmail.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BaseTypeComponent,
+    SignInComponent,
+    EmailListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [GmailService, FormsModule, BaseTypeComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
