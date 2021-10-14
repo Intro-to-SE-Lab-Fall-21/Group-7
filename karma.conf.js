2015573445bf93e95dcc47c8143cd6101920ac50
Jonathan Harjono <59001247+JoSky21@users.noreply.github.com>
2021-10-14 00:26:00 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -33,17 +33,17 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
