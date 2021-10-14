1440dcc772ede04be5acdddf4a65edb317de72b8
Jonathan Harjono <59001247+JoSky21@users.noreply.github.com>
2021-10-13 23:46:28 -0500

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -33,12 +33,6 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
