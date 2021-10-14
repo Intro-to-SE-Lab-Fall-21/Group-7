3bc0fc6e7fc63f40d361df0d8865d70dc6fc2a27
Jonathan Harjono <59001247+JoSky21@users.noreply.github.com>
2021-10-14 11:34:20 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -37,11 +37,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadlessCI'],
+    browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--remote-debugging-port=9222']
       }
     },
     singleRun: false,
